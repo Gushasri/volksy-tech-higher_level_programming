0354943ac3f906cb3cba54fad7f91c0e9909b724
Gushasri <ushasrig@brightcomgroup.com>
2023-01-04 08:06:42 -0800

    usha

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if _name_ == "_main_":
+    import sys
+    length = len(sys.argv)
+    if length <= 1:
+        print("0 arguments.")
+    elif length == 2:
+        print("{:d} argument:".format(length - 1))
+    else:
+        print("{:d} arguments:".format(length - 1))
+    for i in range(1, length):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
