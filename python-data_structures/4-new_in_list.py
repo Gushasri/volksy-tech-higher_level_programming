c61796b078b45b80f69fd55c28fa766179805512
Gushasri <ushasrig@brightcomgroup.com>
2023-01-05 21:43:31 -0800

    4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
         return my_list
-    my_list(idx) = element
     new_list = my_list
+    my_list(idx) = element
     return new_list
