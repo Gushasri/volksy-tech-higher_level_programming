205c646911cb5b4d04d44932118122952e76943f
Gushasri <ushasrig@brightcomgroup.com>
2023-01-10 03:16:40 -0800

    new file

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -2,7 +2,7 @@
 def safe_function(fct, *args):
      import sys
     try:
-         return fct(*args)
+        return fct(*args)
     except Exception as e:
-          print("Exception: " + str(e), file=sys.stderr)
+        print("Exception: " + str(e), file=sys.stderr)
     return None
