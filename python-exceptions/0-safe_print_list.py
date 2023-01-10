4a3fc07c6d9bac19df7d10a75accb970d6e5d41c
Gushasri <ushasrig@brightcomgroup.com>
2023-01-09 20:39:44 -0800

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    for i in range(0, x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            i = i + 1
-        except:
-            continue
+    count = 0
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+            count = count + 1
+    except:
+        print()
+        return count
+    else:
+        print()
+        return count
