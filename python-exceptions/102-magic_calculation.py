eab777c29e265871849a73898dbc48abf570ba90
Gushasri <ushasrig@brightcomgroup.com>
2023-01-10 03:25:44 -0800

    new file

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -8,6 +8,6 @@ def magic_calculation(a, b):
             else:
                 result = result + (a**b) / i
         except:
-           result = b + a
-           break
+            result = b + a
+            break
     return result
