e9fd3c5c5ac619aac71daca6121c32f6bb5655a8
Gushasri <ushasrig@brightcomgroup.com>
2023-01-11 20:54:00 -0800

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+'''size class for square'''
+
+
 class Square(self):
     ''' second program'''
-
-
     def __init__(self, size):
         ''' ajbcuojbujv'''
         self.__size = size
