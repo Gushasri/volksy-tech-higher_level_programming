7973a68f64a0a91937e515d7519908224c2e62ab
Gushasri <ushasrig@brightcomgroup.com>
2023-01-11 23:01:23 -0800

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""string"""
+
+
+class Square:
+    """string size"""
+
+    def _init_(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self._size * self._size
