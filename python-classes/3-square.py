faa054395f8b4fd3a275e7887f413d457c1010d8
Gushasri <ushasrig@brightcomgroup.com>
2023-01-11 22:32:18 -0800

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
+"""area of a square"""
+
+
 class Square:
-    """Defines a square"""
-
-
-    def __init__(self, size=0):
-        """Initialises the data"""
-
-
-        self.__size = size
-        if type(size) != int:
+    """square size"""
+    def _init_(self, size=0):
+        """constructor"""
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
+        self .__size = size
     def area(self):
-        """Returns current square area"""
-
-
-        return self.__size**2
+        """area of square"""
+        return (self._size * self._size)
