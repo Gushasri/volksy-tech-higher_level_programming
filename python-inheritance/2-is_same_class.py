9a7f9b7c32edc1ba26a4f724a5aedee0f8cefd05
Gushasri <ushasrig@brightcomgroup.com>
2023-01-13 01:03:01 -0800

    2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''jvuehdvueh8eihde'''
+
+
+def is_same_class(obj, a_class):
+    '''vnufeducf'''
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
