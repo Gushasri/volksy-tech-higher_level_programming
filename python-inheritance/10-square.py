1cb5b646edf0928fbca668547fe21fa60fe44b2a
Gushasri <ushasrig@brightcomgroup.com>
2023-01-13 01:46:23 -0800

    10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
 """
-===================================
-module with class BaseGeometry
-===================================
+lvsicxygbiwo
 """
 
 
