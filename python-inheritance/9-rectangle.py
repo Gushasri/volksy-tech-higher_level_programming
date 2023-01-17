0ff40063d94a41267035f84347876366ccefd028
Gushasri <ushasrig@brightcomgroup.com>
2023-01-16 21:16:22 -0800

    9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
+"""initialize module"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
+    """create class and inheritance BaseGeometry"""
 
     def __init__(self, width, height):
-        """Method for initialized the attrubutes"""
+        """instancy"""
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
 
     def area(self):
-        """Method to redefine a area method in the parent class"""
-
+        """create Area"""
         return self.__width * self.__height
 
     def __str__(self):
-        """__str__ method for return the next string"""
-
+        """str"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
