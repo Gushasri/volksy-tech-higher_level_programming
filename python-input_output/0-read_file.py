da314a418b1aa54995044bcdb2e6b0156bf6e754
Gushasri <ushasrig@brightcomgroup.com>
2023-01-17 21:23:53 -0800

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,7 +4,7 @@
 
 def read_file(filename=""):
     '''reading usha file'''
-    with open(filename,"r") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end=' ')
 
         
