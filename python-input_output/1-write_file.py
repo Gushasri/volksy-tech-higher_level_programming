999c9f03d1173ae4ac080c758e17a8139d951a73
Gushasri <ushasrig@brightcomgroup.com>
2023-01-17 23:10:56 -0800

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,6 +7,8 @@ def write_file(filename="", text=""):
     with open(filename, encoding='utf-8')as file:
         ccount = 0
         line = file.readline()
-        for line in f:
+        for line in f: 
+            if not line:
+                break
             ccount = ccount+len(line)
     return ccount
