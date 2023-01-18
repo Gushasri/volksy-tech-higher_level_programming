50932d1e5fcdfba40871e4d2137073ce1d77420c
Gushasri <ushasrig@brightcomgroup.com>
2023-01-18 03:26:02 -0800

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-'''ushasri'''
+"""0x0B. Python - Input/Output, task 1. Number of lines """
 
 
-def write_file(filename="", text=""):
-    '''ushasri'''
-    with open(filename, encoding='utf-8')as file:
-        ccount = 0
-        line = file.readline()
-        for line in f: 
+def number_of_lines(filename=""):
+    """Returns the number of lines in a text file.
+    Args:
+        filename (str): name of file to be opened
+    """
+    line_count = 0
+    with open(filename, encoding='utf-8') as file:
+        while True:
+            line = file.readline()
             if not line:
                 break
-            ccount = ccount+len(line)
-    return ccount
+            line_count += 1
+    return line_count
