43eb1acb46d1a720461e3af12a989015ae70964a
Gushasri <ushasrig@brightcomgroup.com>
2023-01-17 23:03:25 -0800

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+'''ushasri'''
+
+
 def write_file(filename="", text=""):
+    '''ushasri'''
     with open(filename, encoding='utf-8')as file:
         ccount = 0
         for line in f:
         ccount = ccount+len(line)
     return ccount
-        
