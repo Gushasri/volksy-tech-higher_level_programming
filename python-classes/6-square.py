bbe914b7fd57f8ea23b87ca17c0e8ccef4ab51e7
Gushasri <ushasrig@brightcomgroup.com>
2023-01-18 23:10:09 -0800

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -102,3 +102,5 @@ class Square:
                 for col in range(0, self.__size):
                     print("#", end="")
                     print()
+
+#        print("size: {} position: {}".format(self.__size, self.__position))
