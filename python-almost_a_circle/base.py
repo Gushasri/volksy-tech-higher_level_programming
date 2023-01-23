a9e83c5ed223b9e30b773a358f5d3ee96f3ed981
Gushasri <ushasrig@brightcomgroup.com>
2023-01-23 03:25:17 -0800

    new file

---
+++ python-almost_a_circle/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Base module contains class Base """
+
+
+class Base():
+    """ base clase for checking id for other classes """
+
+    __nb_objects = 0
+
+    def _init_(self, id=None):
+        """ initialization base class with id """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
