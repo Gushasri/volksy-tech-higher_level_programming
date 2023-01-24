5bc0501ce97294c01e07e80d8d36c0fb9ddc7c8f
Gushasri <ushasrig@brightcomgroup.com>
2023-01-24 00:59:29 -0800

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,16 +1,83 @@
 #!/usr/bin/python3
-""" Base module contains class Base """
+"""base Module"""
+import json
 
 
-class Base():
-    """ base clase for checking id for other classes """
-
+class Base:
+    """Base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ initialization base class with id """
+        """Initializer for Base class"""
         if id is not None:
             self.id = id
-        else:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of a
+        list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        if type(list_dictionaries) != list:
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+        for dic in list_dictionaries:
+            if type(dic) is not dict:
+                err = "list_dictionaries must be a list of dictionaries"
+                raise TypeError(err)
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of a
+        list of instances to a file
+        """
+        filename = cls._name_ + ".json"
+        dicList = []
+        if list_objs is not None:
+            for obj in list_objs:
+                dicList.append(obj.to_dictionary())
+
+        with open(filename, mode='w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(dicList))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns a list of the JSON string representation.
+        """
+        if json_string is None or (len(json_string) == 0):
+            return([])
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns an instance with all attributes already
+        set.
+        """
+        if cls._name_ == "Rectangle":
+            dummy = cls(1, 1, 0, 0, 'a')
+        if cls._name_ == "Square":
+            dummy = cls(1, 0, 0, 'b')
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances from a .json file"""
+        filename = cls._name_ + ".json"
+        try:
+            with open(filename, mode='r', encoding='utf-8') as f:
+                json_string = f.read()
+                dicList = []
+                for obj in cls.from_json_string(json_string):
+                    dicList.append(cls.create(**obj))
+        except:
+            dicList = []
+        return dicList
