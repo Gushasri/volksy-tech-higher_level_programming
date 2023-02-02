a986af61212c873f6221ce0eaca2606fea7e693e
Gushasri <ushasrig@brightcomgroup.com>
2023-02-02 03:29:54 -0800

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- fidcciyde75sfz.
-SELECT c.id,c.name,s.name FROM city AS c INNER JOIN states AS s ON s.id=c.state_id
+SELECT c.id,c.name,s.name FROM cities AS c INNER JOIN states AS s ON s.id=c.state_id
 ORDER BY c.id;
