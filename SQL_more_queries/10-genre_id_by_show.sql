51c3bdd5f41135e12879f3e01f3c818b03bff2dc
Gushasri <ushasrig@brightcomgroup.com>
2023-02-03 03:09:34 -0800

    10-genre_id_by_show.sql

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- yhff76do56r9;7ydwl<F6>85.
+
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
+
