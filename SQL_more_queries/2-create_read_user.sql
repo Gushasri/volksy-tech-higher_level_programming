85876f9543d6cb071be8d6d5c1d123514848f6c7
Gushasri <ushasrig@brightcomgroup.com>
2023-02-02 23:19:23 -0800

    2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,10 @@
 -- lwldeekcdc.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+CREATE USER IF NOT EXISTS user_0d_2@localhost
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
    ON hbtn_0d_2.*
-   TO 'user_0d_2'@'localhost'
+   TO user_0d_2@localhost
    IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
