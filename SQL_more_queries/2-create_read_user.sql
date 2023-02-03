46ff4f0358a2f7d1b19a19a1eb8755097dbe72c3
Gushasri <ushasrig@brightcomgroup.com>
2023-02-02 23:21:20 -0800

    2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -5,5 +5,4 @@ CREATE USER IF NOT EXISTS user_0d_2@localhost
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
    ON hbtn_0d_2.*
-   TO user_0d_2@localhost
-   IDENTIFIED BY 'user_0d_2_pwd';
+   TO user_0d_2@localhost;
