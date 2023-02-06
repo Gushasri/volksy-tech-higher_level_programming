b70bcc44072bdf935fd1d02faf573cda7ba4b7c5
Gushasri <ushasrig@brightcomgroup.com>
2023-02-05 22:27:17 -0800

    new file

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,12 +1,7 @@
 -- Lists all comedy shows in the database hbtn_0d_tvshows.
--- Records are ordered by descending show title.
-SELECT t.`title`
-  FROM `tv_shows` AS t
-       INNER JOIN `tv_show_genres` AS s
-       ON t.`id` = s.`show_id`
-
-       INNER JOIN `tv_genres` AS g
-       ON g.`id` = s.`genre_id`
-       WHERE g.`name` = "Comedy"
- ORDER BY t.`title`;
-
+SELECT tv_shows.title, tv_genres.name  FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name ASC;
