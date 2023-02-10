4451f2d2d03ba879c2a461cd7ce9f5d096eb6fa9
Gushasri <ushasrig@brightcomgroup.com>
2023-02-10 03:18:20 -0800

    new fiel

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-'''vdvrddfwwdxd vv'''
-
-import sys
+"""
+takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument,
+safe from MySQL injections
+"""
 import MySQLdb
+from sys import argv
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
-
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name=%s ORDER BY states.id"
+    num_rows = cur.execute(sql, (argv[4], ))
+    for i in range(num_rows):
+        print(cur.fetchone())
