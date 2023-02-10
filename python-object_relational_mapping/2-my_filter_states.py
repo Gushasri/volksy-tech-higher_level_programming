cad62efa5d9dcbe3ca96cc8654bf97544cf7ce9d
Gushasri <ushasrig@brightcomgroup.com>
2023-02-10 03:10:18 -0800

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
-import sys
-import MySQLdb
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+'''fuydutd'''
+def read_lines(filename="", nb_lines=0):
+    """reads n lines of a text file
+    Args:
+        filename(str): filename
+        nb_lines(int): number of lines in file
+    """
+    i = 0
+    with open(filename, mode="r", encoding="utf-8") as a_file:
+        if nb_lines <= 0:
+            print(a_file.read(), end="")
+        else:
+            for a_line in a_file:
+                if i < nb_lines:
+                    print(a_line, end="")
+                i += 1
