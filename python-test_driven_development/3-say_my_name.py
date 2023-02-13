4cf0f2d53e66ec6539a72ded77e41d5a2279a468
Gushasri <ushasrig@brightcomgroup.com>
2023-02-12 23:13:35 -0800

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -18,4 +18,3 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {:s} {:s}".format(first_name, last_name))
-
