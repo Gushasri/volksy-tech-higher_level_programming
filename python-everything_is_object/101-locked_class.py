bd78759901c463a710601c82ffe48d3c3a6f8ba6
Gushasri <ushasrig@brightcomgroup.com>
2023-02-13 23:12:31 -0800

    101-locked_class.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+# 101-locked_class.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a locked class."""
+
+
 class LockedClass:
-    __slots__ = ['first_name']
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
