4ff78d2875a283252ddbcd19f81323ccfe00a9bf
Gushasri <ushasrig@brightcomgroup.com>
2023-02-13 21:26:30 -0800

    d

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,20 @@
-#!/usr/bin/python3
-import json
-"""JavaScript Object Notation"""
-
-
-def save_to_json_file(my_obj, filename):
-    """writes an Object to a text file, using a JSON representation
-    Args:
-        my_obj(obj): object
-        filename(str): filename
-    """
-    with open(filename, mode="w", encoding="utf-8") as a_file:
-        a_file.write(json.dumps(my_obj))
+#!/usr/bin/python3                                                                                                                                                             
+"""                                                                                                                                                                            
+Script that adds all arguments to a Python list,                                                                                                                               
+then saves them to a file                                                                                                                                                      
+"""                                                                                                                                                                            
+import sys                                                                                                                                                                     
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file                                                                                                        
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file                                                                                                  
+                                                                                                                                                                               
+fname = "add_item.json"                                                                                                                                                        
+List = []                                                                                                                                                                      
+                                                                                                                                                                               
+try:                                                                                                                                                                           
+    List = load_from_json_file(fname)                                                                                                                                          
+except:                                                                                                                                                                        
+    pass                                                                                                                                                                       
+                                                                                                                                                                               
+for i in range(1, len(sys.argv)):                                                                                                                                              
+    List.append(sys.argv[i])
+save_to_json_file(list,fname)
