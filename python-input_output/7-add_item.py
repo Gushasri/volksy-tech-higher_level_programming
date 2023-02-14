b48eaaac49ad5baf0f06300c39e7bb78ad96521e
Gushasri <ushasrig@brightcomgroup.com>
2023-02-13 21:14:41 -0800

    new file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 '''wsfrddsd'''
 
+
 from sys import argv
 """access commandline arguments"""
+
+
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 """create object from JSON file"""
+
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 """writes an object to text file, using JSON representation"""
+
 
 filename = "add_item.json"
 try:
