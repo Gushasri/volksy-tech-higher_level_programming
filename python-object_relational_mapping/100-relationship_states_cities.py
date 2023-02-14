25edc5b23620a3fcf285ee2d28b1aca86e164ab9
Gushasri <ushasrig@brightcomgroup.com>
2023-02-13 23:03:34 -0800

    100-relationship_states_cities.py

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 '''uhfcisjdxoww'''
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''ouschs0ihiwx'''
+''' Defines a City model.
+ Inherits from SQLAlchemy Base and links to the MySQL table cities.
+'''
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -16,4 +18,4 @@ class City(Base):
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)#!/usr/bin/python3
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
